9f9400fe36f68a39266f806afeb048f29ece4bd8
coytar <raghavendra624@gmail.com>
2018-07-29 10:34:52 +1000

    Create NodeMCU.ino

---
+++ NodeMCU_To_Nano/NodeMCU.ino
@@ -0,0 +1,98 @@
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+
+const char* ssid = "WifiAccessPoint";
+const char* password = "Password";
+
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
+
+// Software serial port
+SoftwareSerial swSer(D1, D2);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+  // Start software serial  
+  swSer.begin(9600);
+  swSer.write('1');    
+  swSer.write('1');    
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+  
+  // Match the request
+  int val;
+  if (req.indexOf("/gpio/0") != -1)
+    val = 0;
+  else if (req.indexOf("/gpio/1") != -1)
+    val = 1;
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  if (val == 0) {
+    swSer.write('0');    
+  }
+  else if (val == 1) {
+    swSer.write('1');    
+  }
+    
+  client.flush();
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
+  s += (val)?"high":"low";
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
+}
